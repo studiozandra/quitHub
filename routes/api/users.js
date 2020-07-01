084baee6346a28aadc7d2e60d144e66d56b0490b
studiozandra <zan@studiozandra.com>
2020-07-01 09:46:14 -1000

    add user input validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,12 +3,25 @@ const express = require('express');
 // Use Express router
 const router = express.Router();
 
+// Validate user's entered data (rules on express-validator at github)
+const { check, validationResult } = require('express-validator');
+
 // no "app.get", instead "router"
 // type of req and endpoint:
 // @route POST api/users 
 // @desc Register user -- we need to send name, email ,and password
 // @access Public (no token needed)
-router.post('/', (req, res) => {
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(), 
+    check('email', 'Please include a valid email').isEmail(), 
+    check('password', 'Please enter a password with 6 or more chars').isLength({ min: 6 })
+    ], 
+    (req, res) => {
+    const errors = validationResult(req);
+    // if there are errors
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
     console.log(req.body);
     res.send('User route')
 }); // test route, set up in server.js (defined routes section). We will POST req to create new users
