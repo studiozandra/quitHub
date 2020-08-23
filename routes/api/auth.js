dd3abb73ba34a097d03d762ebfdb5aa6aa4fa8dd
studiozandra <zan@studiozandra.com>
2020-08-22 15:42:43 -1000

    add protected route parameter, Postman tests ok

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 // Use Express router
 const router = express.Router();
+// use our custom middleware
+const auth = require('../../middleware/auth'); // ..outside of api, ..outside of routes, into /middleware/auth
 
 // no "app.get", instead "router"
 // type of req and endpoint:
 // @route GET api/auth 
 // @desc Test route
 // @access Public (no token needed)
-router.get('/', (req, res) => res.send('auth test route')); // test route
+router.get('/', auth, (req, res) => res.send('auth test route')); // test route. With middleware, we added 'auth,' as second parameter to make protected route.
 
 module.exports = router;
